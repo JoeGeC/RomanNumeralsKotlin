b67d376af1e329246899a690b1392bff33d2aa0d
joeb <joe.barker@mkodo.com>
2021-09-07 15:20:59 +0100

    Converts 3 to III

--- src/main/kotlin/com/mkodo/romannumerals/RomanNumerals.kt
+++ src/main/kotlin/com/mkodo/romannumerals/RomanNumerals.kt
@@ -2,8 +2,8 @@ package com.mkodo.romannumerals
 
 class RomanNumerals {
     fun convert(number: Int): String {
-        if(number == 1)
-            return "I"
-        return "II"
+        var result = ""
+        for (i in 1..number) result += "I"
+        return result
     }
 }
--- src/test/kotlin/com/mkodo/romannumerals/RomanNumeralsShould.kt
+++ src/test/kotlin/com/mkodo/romannumerals/RomanNumeralsShould.kt
@@ -4,14 +4,20 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class RomanNumeralsShould {
+    private val romanNumerals = RomanNumerals()
+
     @Test
     fun `Convert 1 to I`() {
-        val romanNumerals = RomanNumerals()
         assertEquals("I", romanNumerals.convert(1))
     }
+
     @Test
     fun `Convert 2 to II`() {
-        val romanNumerals = RomanNumerals()
         assertEquals("II", romanNumerals.convert(2))
+    }
+
+    @Test
+    fun `Convert 3 to III`() {
+        assertEquals("III", romanNumerals.convert(3))
     }
 }
