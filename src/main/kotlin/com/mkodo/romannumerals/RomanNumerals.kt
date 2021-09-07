51ffe8830a0025dfb16c4196b49b9a707dfe41b5
joeb <joe.barker@mkodo.com>
2021-09-07 16:26:36 +0100

    Added mapping of number to numeral

--- src/main/kotlin/com/mkodo/romannumerals/RomanNumerals.kt
+++ src/main/kotlin/com/mkodo/romannumerals/RomanNumerals.kt
@@ -5,16 +5,27 @@ class RomanNumerals {
         var count = number
         var result = ""
 
-        if (count >= 10) {
-            result += "X"
+        while (count >= 10) {
+            result += numeralFor(10)
             count -= 10
         }
 
-        if (count >= 5) {
-            result += "V"
+        while (count >= 5) {
+            result += numeralFor(5)
             count -= 5
         }
-        for (i in 1..count) result += "I"
+
+        while (count >= 1) {
+            result += numeralFor(1)
+            count -= 1
+        }
+
         return result
+    }
+
+    private fun numeralFor(i: Int): String {
+        if (i == 10) return "X"
+        if (i == 5) return "V"
+        return "I"
     }
 }
--- src/test/kotlin/com/mkodo/romannumerals/RomanNumeralsShould.kt
+++ src/test/kotlin/com/mkodo/romannumerals/RomanNumeralsShould.kt
@@ -7,42 +7,21 @@ class RomanNumeralsShould {
     private val romanNumerals = RomanNumerals()
 
     @Test
-    fun `Convert 1 to I`() {
+    fun `convert units of 1`() {
         assertEquals("I", romanNumerals.convert(1))
-    }
-
-    @Test
-    fun `Convert 2 to II`() {
         assertEquals("II", romanNumerals.convert(2))
-    }
-
-    @Test
-    fun `Convert 3 to III`() {
         assertEquals("III", romanNumerals.convert(3))
-    }
-
-    @Test
-    fun `Convert 5 to V`() {
         assertEquals("V", romanNumerals.convert(5))
-    }
-
-    @Test
-    fun `Convert 6 to VI`() {
         assertEquals("VI", romanNumerals.convert(6))
-    }
-
-    @Test
-    fun `Convert 7 to VII`() {
         assertEquals("VII", romanNumerals.convert(7))
     }
 
     @Test
-    fun `Convert 10 to X`() {
+    fun `convert units of 10`() {
         assertEquals("X", romanNumerals.convert(10))
+        assertEquals("XI", romanNumerals.convert(11))
+        assertEquals("XV", romanNumerals.convert(15))
+        assertEquals("XX", romanNumerals.convert(20))
     }
 
-    @Test
-    fun `Convert 11 to XI`() {
-        assertEquals("XI", romanNumerals.convert(11))
-    }
 }
